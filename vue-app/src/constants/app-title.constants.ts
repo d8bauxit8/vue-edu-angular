78bebd8aa56936929b87a35a30da9da455e79795
Mark Jehoda <mark.jehoda@proton.me>
2023-11-26 12:43:00 +0100

    add page not found view and set app title with meta lib

---
+++ vue-app/src/constants/app-title.constants.ts
@@ -0,0 +1,1 @@
+export const APP_TITLE: string = 'Vue3 workshop @ 2023';
---
+++ vue-app/src/views/PageNotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center">
+    <h2 class="mb-4 mt-16 text-8xl text-red drop-shadow-2xl">The page not found</h2>
+  </div>
+</template>
